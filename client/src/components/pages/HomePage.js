ac5ff861136ac3aec771264b09a199043b67b522
juliusvdw <juliusvdw@gmail.com>
2021-10-19 14:13:39 +0800

    Hero text section complete

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -4,7 +4,12 @@ const HomePage = () => {
     return (
         <div id = 'hero-section' style = {heroStyle}>
               <div className = 'container' id = 'hero-section-container'>
-                  <div id = 'hero-text-container'>
+                  <div id = 'hero-text-container' style = {textContainerStyle}>
+                      <p style = {heroLightHeading} className = 'mb-3'>NEW PRODUCT</p>
+                      <h1 style = {heroProductHeading} className = 'mb-3'> XX99 MARK 11 HEADPHONES</h1>
+                      <p style = {heroSubtextStyle} >Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                      <div className = 'btn btn-primary btn-lg btn-light-custom d-flex'>SEE PRODUCT </div>
+
                       
                   </div>
               </div>
@@ -15,6 +20,38 @@ const HomePage = () => {
 export default HomePage;
 
 const heroStyle = {
-    backgroundColor : '#191919',
+    background : '#191919',
     height:'720px'
+}
+
+const textContainerStyle = {
+    width:'400px',
+    paddingTop:'130px'
+}
+
+const heroLightHeading = {
+    letterSpacing:'10px',
+    color:'#FFFFFF',
+    opacity:'0.4',
+    fontSize:'14px'
+}
+
+const heroProductHeading = {
+    fontWeight:'bold',
+    color:'#FFFFFF',
+    fontSize:'54px',
+    letterSpacing :'2px'
+}
+const heroSubtextStyle = {
+    fontWeight:'light',
+    color:'#FFFFFF',
+    opacity:'0.6',
+    fontSize:'15px',
+    lineHeight:'25px',
+    width:'85%',
+    marginBottom:'45px'
+}
+
+const heroImageStyle = {
+    position:'absolute'
 }
