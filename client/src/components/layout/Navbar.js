33c7e5c5ec0ddaa9ceda31885a33f50718644fe5
juliusvdw <juliusvdw@gmail.com>
2021-10-19 13:12:06 +0800

    Navbar component complete + implemented

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,56 @@
+import React from'react';
+
+const Navbar = () => {
+    return (
+
+       
+        <nav className ="navbar navbar-expand-lg  " style = {navStyle}>
+            <div className = 'container p-0' style = {navContainerStyle}>
+  <a className ="navbar-brand" href="#"><img src = '/assets/shared/desktop/logo.svg'></img></a>
+  <button className ="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className ="navbar-toggler-icon"></span>
+  </button>
+  <div className ="collapse navbar-collapse" id="navbarNav">
+    <ul className ="navbar-nav mx-auto" style = {navLinkStyle}>
+      <li className ="nav-item text-white">
+        <a className ="nav-link text-white" href="#">HOME </a>
+      </li>
+      <li className ="nav-item">
+        <a className ="nav-link text-white" href="#">HEADPHONES</a>
+      </li>
+      <li className ="nav-item">
+        <a className ="nav-link text-white" href="#">SPEAKERS</a>
+      </li>
+      <li className ="nav-item">
+        <a className ="nav-link text-white" href="#">EARPHONES</a>
+      </li>
+    </ul>
+  </div>
+  <div className = 'ml-auto'> <i className=" bi-cart "style = {{color:'white', fontSize:'24px'}} />
+</div>
+  </div>
+</nav>
+    )
+}
+
+const navStyle = {
+    backgroundColor :'#191919',
+    height:'105px',
+}
+
+const navLinkStyle =  {
+    letterSpacing:'2px',
+    fontSize : '13px',
+    fontWeight:'bold',
+    justifyContent:'space-around',
+    width:'460px',
+    color:'#FFFFFF'
+
+}
+
+const navContainerStyle = {
+    borderBottom :'1px solid rgba(151,151,151, .2)',
+    height:'95px'
+}
+
+export default Navbar;
