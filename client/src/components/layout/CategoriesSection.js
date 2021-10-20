0fd4a1e7ff6e34868057b598ceb29b80631c2a5c
juliusvdw <juliusvdw@gmail.com>
2021-10-20 15:40:36 +0800

    Categories section layout complete + category image and tet container styling complete

--- client/src/components/layout/CategoriesSection.js
+++ client/src/components/layout/CategoriesSection.js
@@ -2,20 +2,46 @@ import React from "react";
 
 const CategoriesSection = () => {
     return (
-        <div className = 'container'>
+        <div className = 'container' style = {containerStyle}>
             <div className = 'row'>
-                <div className = 'col-lg-4'>
-                    Test
+                <div className = 'col-lg-4 home-category-box' >
+                    <div className = 'home-category-box' style = {categoryBoxStyle}>
+                        <div className = 'home-category-image-container w-100'>
+                            <img src = '/assets/shared/desktop/image-headphones.png' style = {categoryImageStyle}/>
+                        </div>
+
+                    </div>
                 </div>
-                <div className = 'col-lg-4'>
-                    test
+                <div className = 'col-lg-4 home-category-box' >
+                    <div className = 'home-category-box' style = {categoryBoxStyle}>
+
+                    </div>
                 </div>
-                <div className = 'col-lg-4'>
-                    test
+                <div className = 'col-lg-4 home-category-box' >
+                    <div className = 'home-category-box' style = {categoryBoxStyle}>
+
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+const containerStyle = {
+    marginTop:'230px'
+}
+const categoryBoxStyle = {
+    height:'200px',
+    backgroundColor :'#f1f1f1',
+    position:'relative'
+}
+const categoryImageStyle = {
+    width:'250px',
+    transform:'translate(-50%,-50%)',
+    left:'50%',
+    position:'absolute',
+    top:'30px'
+}
+
 
 export default CategoriesSection
