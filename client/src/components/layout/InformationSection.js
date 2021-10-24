48731b19c6cbe83ef9a8c1906865011e330bc584
juliusvdw <juliusvdw@gmail.com>
2021-10-24 16:38:07 +0800

    logic implemented for conditional rendering of image based on device size

--- client/src/components/layout/InformationSection.js
+++ client/src/components/layout/InformationSection.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive'
+
 
 const InformationSection = () => {
+
+    //Set media query for tablet size 
+    const isTablet  = useMediaQuery({ maxWidth: 1024 })
+    const isDesktop  = useMediaQuery({ minWidth: 1024 })
+
+    //Determine which hero image to be displayed based on device
+
+    let imageLink;
+    if(isDesktop){
+        imageLink = '/assets/shared/desktop/image-best-gear.jpg'
+    } else if (isTablet){
+        imageLink = '/assets/shared/tablet/image-best-gear.jpg'
+
+    }
+
 return (
      <div className = 'container' style = {containerStyle}>
          <div className = 'row h-100 '>
-             <div className = 'col-lg-6'>
-                 <div className = 'd-flex flex-column h-100 ' style = {informationTextStyle} >
+             <div id = 'information-text-section' className = 'col-lg-6'>
+                 <div id = 'information-text-container' className = 'd-flex flex-column h-100 ' style = {informationTextStyle} >
                     <h1 className = 'mb-4' style = {informationHeadingStyle}>BRINGING YOU THE <span style = {{color:'#d87d4a'}}>BEST</span> AUDIO GEAR</h1>
                     <p style = {informationSubtextextStyle}>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
                  </div>
                  
              </div>
-             <div className = 'col-lg-6'>
+             <div id = 'information-image-section' className = 'col-lg-6'>
                  <div >
-                     <img className = 'img-fluid w-100' src = '/assets/shared/desktop/image-best-gear.jpg' style = {{borderRadius:'8px'}}/>
+                     <img className = 'img-fluid w-100' src = {`${imageLink}`} style = {{borderRadius:'8px'}}/>
                  </div>
 
              </div>
