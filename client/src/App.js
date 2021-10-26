a1aecba237b79218344e8c44624a9ca321dfb987
juliusvdw <juliusvdw@gmail.com>
2021-10-26 11:48:48 +0800

    Installed react-router-dom + implemented on app.js + created productcategory page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 
 import './App.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
 
 import HomePage from './components/pages/HomePage'
+import ProductCategoryPage from './components/pages/ProductCategoryPage'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 
@@ -11,9 +14,18 @@ import Footer from './components/layout/Footer'
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
-      <HomePage />
+
+      <Switch>
+
+      <Route exact path = {'/'} render  = {(props) => <HomePage {...props} />}/>
+      <Route exact path = {'/category'} render  = {(props) => <ProductCategoryPage {...props} />}/>
+
+      </Switch>
+
       <Footer />
+      </Router>
       
     </div>
   );
