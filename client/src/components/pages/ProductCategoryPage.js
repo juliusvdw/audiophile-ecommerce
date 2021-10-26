0ce3df9f9e05e311d773f12b6ce775333d5dcebd
juliusvdw <juliusvdw@gmail.com>
2021-10-26 14:11:26 +0800

    CAtegory page header section + styling complete

--- client/src/components/pages/ProductCategoryPage.js
+++ client/src/components/pages/ProductCategoryPage.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const CategoryPage = () => {
 return (
-    <h1>Hello world</h1>
+    <div style = {headerContainerStyle}>
+        <h1 style = {headerStyle}>HEADPHONES</h1>
+    </div>
 )
+}
+
+const headerContainerStyle = {
+    height:'239px',
+    backgroundColor:'#191919',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    
+}
+
+const headerStyle = {
+    color:'#FFFFFF',
+    fontSize:'40px',
+    letterSpacing:'1.5px'
 }
 
 export default CategoryPage
