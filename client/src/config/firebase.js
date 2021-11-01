cff13648662a98af02d00f91682d070e1b23a0ce
juliusvdw <juliusvdw@gmail.com>
2021-11-01 21:41:25 +0800

    Firebaase config complete

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,9 +1,7 @@
-
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "@firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyAW__I4T72XR5EBw-2nVtesV_zTKCAjT4U",
   authDomain: "audiophile-fb62b.firebaseapp.com",
@@ -12,9 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "576346801689",
   appId: "1:576346801689:web:ff4fcc405ab4aab8670c88",
   measurementId: "G-19R8ZNTW81"
-};
 
-const app  = initializeApp(firebaseConfig)
+}
 
+const app = initializeApp(firebaseConfig);
 
-export default getFirestore();
+export const db = getFirestore(app)
