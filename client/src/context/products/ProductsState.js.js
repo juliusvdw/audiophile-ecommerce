906d5568aab9dd32a0aff8fba76745714ff0c2bb
juliusvdw <juliusvdw@gmail.com>
2021-11-01 21:42:39 +0800

    Get all Products+ get Products by category logic implemented

--- client/src/context/products/ProductsState.js.js
+++ client/src/context/products/ProductsState.js.js
@@ -1,21 +1,53 @@
 import React,{useReducer} from 'react';
+import {db} from '../../config/firebase';
+import {collection, getDocs, where, query} from 'firebase/firestore'
+
 
 import ProductsContext from "./productsContext";
 import productsReducer from "./productsReducer";
+import { GET_ALL_PRODUCTS } from '../Types';
 
 const ProductsState = (props) => {
     const initialState = {
+        products:null,
 
     }
 
     const [state,dispatch] = useReducer(productsReducer,initialState)
 
+    //DB collection refs
+    const productsCollectionRef = collection(db,'product')
+    
+
+
     //Logic
+
+    //Get all products 
+    const getAllProducts = async () => {
+        const data = await getDocs(productsCollectionRef);
+        const products = data.docs.map((doc) => ({...doc.data(), id:doc.id}))
+
+        dispatch({type: GET_ALL_PRODUCTS, payload:products})
+        
+    }
+
+    //Get products by category
+    const getProductsByCategory = async (category) => {
+        console.log(category)
+        const categoryCollection = query(collection(db, "product"), where("category", "==", `${category}`));
+        const data = await getDocs(categoryCollection)
+        const products = data.docs.map((doc) => ({...doc.data(), id:doc.id}));
+
+        console.log(products)
+
+    }
 
     return (
         <ProductsContext.Provider
           value={{
-            
+            products:state.products,
+            getAllProducts,
+            getProductsByCategory,
           }}
         >
           {props.children}
--- client/src/context/products/productsReducer.js
+++ client/src/context/products/productsReducer.js
@@ -1,7 +1,16 @@
 
+import {
+GET_ALL_PRODUCTS,
+} from '../Types'
 
 export default (state,action) => {
     switch(action.type) {
+        case GET_ALL_PRODUCTS:
+            return {
+                ...state,
+                products:action.payload
+            }
+
         default:
         return state
     }
