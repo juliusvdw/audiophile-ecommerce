a235c91138a56b044e213da5cd8bf0be4af40501
juliusvdw <juliusvdw@gmail.com>
2021-11-02 09:20:13 +0800

    Key prop added to url links with params so component remount will take place on same url

--- client/src/App.js
+++ client/src/App.js
@@ -39,8 +39,8 @@ function App() {
       <Switch>
 
       <Route exact path = {'/'} render  = {(props) => <HomePage {...props} />}/>
-      <Route  exact path = {'/category/:category'} render  = {(props) => <ProductCategoryPage {...props} />}/>
-      <Route  path = {'/product/:product'} render  = {(props) => <ProductPage {...props} />}/>
+      <Route  exact path = {'/category/:category'} render  = {(props) => <ProductCategoryPage {...props} key={props.match.params.category}/>}/>
+      <Route  path = {'/product/:product'} render  = {(props) => <ProductPage {...props} key={props.match.params.category}/>}/>
       <Route  path = {'/checkout'} render  = {(props) => <CheckoutPage {...props} />}/>
 
       </Switch>
