8f7ec3a8f4cd64f283be158661ef13f67683aaa8
juliusvdw <juliusvdw@gmail.com>
2021-11-03 09:50:52 +0800

    Implemented loading logic

--- client/src/components/pages/ProductPage.js
+++ client/src/components/pages/ProductPage.js
@@ -15,54 +15,56 @@ const ProductPage = (props) => {
 
     //Init context and destructure needed logic
     const productsContext = useContext(ProductsContext)
-    const {getSingleProduct, singleProduct } = productsContext;
+    const {getSingleProduct, singleProduct,loading } = productsContext;
 
     useEffect(() => {
         getSingleProduct(props.match.params.product)
     }, [])
 
      
-    return (
-        <>
-        <div className = 'container'>
-
-                {singleProduct != null && 
-                <>
-                <Product productData = {{
-                  category : singleProduct[0],
-                  order : singleProduct[0].order,
-                  description : singleProduct[0].description,
-                  id : singleProduct[0].id,
-                  image : singleProduct[0].image,
-                  name : singleProduct[0].name,
-                  isNew : singleProduct[0].new,
-                  price : singleProduct[0].price }}/> 
-
-                  <FeaturesSection productData = {{
-                    features :  singleProduct[0].features,
-                    includes : singleProduct[0].includes,
-
-                  }}/>
-                  
-                  <ThreeImages productData = {{
-                      gallery : singleProduct[0].gallery,
-                  }}/>
-
-                <Recommended productData = {{
-                  others : singleProduct[0].others,
-
-                }}/>
-
-                <CategoriesSection />
-
-                <InformationSection /> 
-                </>
-
-                  }
-
-                </div>
-        </>
-    )
+    if(singleProduct != null && !loading){
+        return (
+            <>
+            <div className = 'container'>
+    
+                    {loading == false && 
+                    <>
+                    <Product productData = {{
+                      category : singleProduct[0],
+                      order : singleProduct[0].order,
+                      description : singleProduct[0].description,
+                      id : singleProduct[0].id,
+                      image : singleProduct[0].image,
+                      name : singleProduct[0].name,
+                      isNew : singleProduct[0].new,
+                      price : singleProduct[0].price }}/> 
+    
+                      <FeaturesSection productData = {{
+                        features :  singleProduct[0].features,
+                        includes : singleProduct[0].includes,
+    
+                      }}/>
+                      
+                      <ThreeImages productData = {{
+                          gallery : singleProduct[0].gallery,
+                      }}/>
+    
+                    <Recommended productData = {{
+                      others : singleProduct[0].others,
+    
+                    }}/>
+    
+                    <CategoriesSection />
+    
+                    <InformationSection /> 
+                    </>
+    
+                      }
+    
+                    </div>
+            </>
+        )
+    } else {return <h1>Loading</h1>}
 }
 
 
